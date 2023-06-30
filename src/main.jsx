bbf634f2f9f37f978c505f00a6d2d0753de9dff5
Nextradigi <nextradigi@gmail.com>
2023-06-30 20:03:05 +0700

    add assets components page store

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Homepage  from './pages/homepage';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import "../src/css/main.css"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <Homepage/>,
   },
 ]);
 
---
+++ src/pages/homepage.jsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+	return (
+		<div className="flex w-screen h-screen justify-center items-center">
+			<button class="btn">Hello daisyUI</button>
+		</div>
+	)
+}
+
+export default Homepage
