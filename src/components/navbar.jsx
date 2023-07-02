7d52be3fd676fdeaf22194c94841053ce3cb1cf7
Nextradigi <nextradigi@gmail.com>
2023-07-02 17:00:31 +0700

    fix homepage

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,27 +1,38 @@
-import user from "../assets/icons/User.svg"
-import logo from "../assets/icons/logo.png"
-import "../css/main.css"
+import user from "../assets/icons/User.svg";
+import logo from "../assets/icons/logo.png";
+import "../css/main.css";
 
 const Navbar = () => {
-	return( 
-		<div className="flex bg-white w-full h-16 flex items-center">
-        	<a className="btn btn-ghost normal-case text-xl ml-4 hover:bg-white">
-				<img src={logo} alt="Logo" style={{ width: '150px', height: 'auto' }} />
-			</a>
-			<div className="flex-none">
-    			<ul className="menu menu-horizontal px-1 text-black font-bold">
-      				<li><a>Home</a></li>
-					<li><a>About Us</a></li>
-					<li><a>Offset</a></li>
-				</ul>
-			</div>
-			<div className="signIn flex items-center">
-				<button className="btn-signIn px-5 py-2 font-xs rounded-full mb-1 ease-linear transition-all duration-150" type="button">
-					<img src={user} alt="user" className="inline-block mr-2 mb-1 icon" /> Sign In 
-				</button>
-			</div>
-	    </div>
-	)
-}
+  return (
+    <div className="flex bg-white w-full h-16 items-center">
+      <a className="btn btn-ghost normal-case text-xl ml-4 hover:bg-white">
+        <img src={logo} alt="Logo" style={{ width: "150px", height: "auto" }} />
+      </a>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1 text-black font-bold">
+          <li className="relative">
+            <a>Home</a>
+	    <span className="absolute border-b-4 border-[#068758] rounded-none w-[70px] bottom-[-15px]"></span>
+          </li>
+          <li>
+            <a>About Us</a>
+          </li>
+          <li>
+            <a>Offset</a>
+          </li>
+        </ul>
+      </div>
+      <div className="signIn flex items-center">
+        <button
+          className="btn-signIn px-5 py-2 font-xs rounded-full mb-1 ease-linear transition-all duration-150"
+          type="button"
+        >
+          <img src={user} alt="user" className="inline-block mr-2 mb-1 icon" />{" "}
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -9,40 +9,58 @@ const Homepage = () => {
   return (
     <div className="bg-container">
       <Navbar />
-      <div className="flex w-[98vw] h-screen">
-        {/* Part 1 */}
-        <div className="bg-img">
-          <img src={homebg} alt="Home Page" />
-        </div>
-        <section className="greenie mx-auto items-center flex flex-wrap">
-          <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
-            <div className="pt-32 sm:pt-0">
-              <p className="text-1lg sm:text-3xl py-3 text-black font-bold">
-                Welcome to
-              </p>
-              <h1 className="text-3xl sm:text-8xl font-bold leading-normal text-black">
-                Greenie
-              </h1>
-              <p className="text-black py-4">Your Carbon Offset Solution!</p>
-              <p className="py-1 text-black">
-                Take a step towards a greener future with Greenie, your trusted
-                partner in carbon offsetting. We believe that every individual
-                and organization has the power to make a positive impact on the
-                planet, and we're here to make it easy for you to do so.
-              </p>
+      <div className="flex flex-col w-[99vw] h-[6748px]">
+        <section className="w-screen h-[100vh] flex justify-between">
+          <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 flex items-center justify-center">
+            <div className="mb-[25vh]">
+              <div>
+                <p className="text-1lg sm:text-3xl  text-black font-bold">
+                  Welcome to
+                </p>
+              </div>
+
+              <div>
+                <h1 className="text-3xl sm:text-8xl font-bold leading-normal text-black">
+                  Greenie
+                </h1>
+              </div>
+
+              <div className="mb-3 mt-5">
+                <p className="text-black font-bold ">
+                  Your Carbon Offset Solution!
+                </p>
+              </div>
+
+              <div className="w-[500px]">
+                <p className=" text-black">
+                  Take a step towards a greener future with Greenie, your
+                  trusted partner in carbon offsetting. We believe that every
+                  individual and organization has the power to make a positive
+                  impact on the planet, and we're here to make it easy for you
+                  to do so.
+                </p>
+              </div>
+
+              <div className="flex items-center justify-around mt-10">
+                <button className="w-[6vw] btn h-[6vh] rounded-3xl bg-[#068758] text-white border-none">
+                  Start now
+                </button>
+                <div>
+                  <p>or</p>
+                </div>
+                <button className="w-[6vw] btn h-[6vh] rounded-3xl bg-[#FFFFFF] text-[#068758] border-slate-200">
+                  Read more
+                </button>
+              </div>
             </div>
-            <div className="homebtn">
-              <button className="btn btn-g">Start now</button>
-              <button className="btn btn-w">Read more</button>
-            </div>
+          </div>
+
+          <div className="bg-slate-100">
+            <img className="w-[913px] h-[700px]" src={homebg} alt="Home Page" />
           </div>
         </section>
 
-        {/* Part 2 */}
-        <section
-          className="stat container mx-auto px-4 pb-32 pt-48 absolute bottom-0"
-          style={{ marginBottom: "6rem" }}
-        >
+        <section className="" style={{ marginBottom: "6rem" }}>
           <div className="w-1/2 flex justify-start">
             <img src={cloud} alt="cloud" className="w-8 h-8 ml-2" />
           </div>
@@ -56,3 +74,32 @@ const Homepage = () => {
 };
 
 export default Homepage;
+
+          // <div className="bg-img">
+          //   <img src={homebg} alt="Home Page" />
+          // </div>
+
+          // <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
+
+          //   <div className="pt-32 sm:pt-0">
+          //     <p className="text-1lg sm:text-3xl py-3 text-black font-bold">
+          //       Welcome to
+          //     </p>
+          //     <h1 className="text-3xl sm:text-8xl font-bold leading-normal text-black">
+          //       Greenie
+          //     </h1>
+          //     <p className="text-black py-4">Your Carbon Offset Solution!</p>
+          //     <p className="py-1 text-black">
+          //       Take a step towards a greener future with Greenie, your trusted
+          //       partner in carbon offsetting. We believe that every individual
+          //       and organization has the power to make a positive impact on the
+          //       planet, and we're here to make it easy for you to do so.
+          //     </p>
+          //   </div>
+
+          //   <div className="homebtn">
+          //     <button className="btn btn-g">Start now</button>
+          //     <button className="btn btn-w">Read more</button>
+          //   </div>
+
+          // </div>
