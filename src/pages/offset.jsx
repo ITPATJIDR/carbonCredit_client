57be39be664ff53c9b19e9bfb4a5aacca4d98b25
Nextradigi <nextradigi@gmail.com>
2023-07-07 08:45:09 +0700

    add design in offset page

--- src/pages/offset.jsx
+++ src/pages/offset.jsx
@@ -1,15 +1,15 @@
-import { useEffect } from "react";
-import Navbar from "../components/navbar"
+import { useState, useEffect } from "react";
+import Navbar from "../components/navbar";
 import { changeMenu } from "../store/features/menu-slice";
 import { useDispatch, useSelector } from "react-redux";
 import { Image28Filp, Image24 } from "../assets/image";
 import ChooseOffset from "../components/chooseOffset";
 
-
 const Offset = () => {
-  const {chooseMenu} = useSelector((state) => state.menu)
+  const { chooseMenu } = useSelector((state) => state.menu);
+  const [offSetState, setOffSetState] = useState("ChooseOffset")
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const checkChooseMenu = () => {
     if (chooseMenu || chooseMenu !== "Offset") {
@@ -17,21 +17,27 @@ const Offset = () => {
     }
   };
 
+  const checkOffsetState = () => {
+    if (offSetState === "ChooseOffset") {
+      return <ChooseOffset />;
+    }
+  };
+
   useEffect(() => {
     checkChooseMenu();
   });
   return (
-    <div className="relative">
+    <div className="relative bg-[#F2F4F8]">
       <Navbar />
-	<div>
-		tst
-	</div>
-      <div className="flex h-[20vh] w-full justify-between absolute top-[80vh]">
-        <img className="w-[10vw]" src={Image28Filp} alt="image28" />
-        <img src={Image24} alt="image28" />
+      <div className="p-20">
+        {checkOffsetState()}
+      </div>
+      <div className="flex h-[20vh] w-full justify-between absolute top-[80vh] bg-[#F2F4F8]">
+        <img className="w-[200px]" src={Image28Filp} alt="image28" />
+        <img className="w-[200px]" src={Image24} alt="image28" />
       </div>
     </div>
   );
 };
 
-export default Offset
+export default Offset;
