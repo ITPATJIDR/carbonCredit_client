559a39cb9a71e80215c8897c2a793a6a86d85b7d
Nextradigi <nextradigi@gmail.com>
2023-07-18 20:13:44 +0700

    fix cal

--- src/components/calculate.jsx
+++ src/components/calculate.jsx
@@ -71,11 +71,7 @@ const Calculate = () => {
     setShowOffsetZone(false);
   };
 
-<<<<<<< HEAD
-  const calCarbon = (Math.round(calResult.data?.attributes.carbon_kg) * 1.5)
-=======
-  const calCarbon = Math.round(calResult.data?.attributes.carbon_kg) * 15;
->>>>>>> e69382bdcf391a9dd3a29fe692e10da3dfb69a95
+  const calCarbon = Math.round(Math.round(calResult.data?.attributes.carbon_kg) * 1.5)
 
   return (
     <div className="card w-[55vw] h-[60vh] bg-white shadow-xl my-[3rem] z-10">
